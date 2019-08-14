1429b3c057f6d465ddf9d30f8df4c41456c377b7
kishorep07 <kishore.prabhakar7@gmail.com>
2019-08-14 20:46:41 +0530

    started working on ORBs

--- display.py
+++ display.py
@@ -4,7 +4,7 @@ from pygame.locals import DOUBLEBUF
 class Display2D(object):
   def __init__(self, W, H):
     pygame.init()
-    self.screen = pygame.display.set_mode((W, H), DOUBLEBUF)
+    self.screen = pygame.display.set_mode((W, H), DOUBLEBUF) #init a screen
     self.surface = pygame.Surface(self.screen.get_size()).convert()
     print(W, H)
 
@@ -13,8 +13,8 @@ class Display2D(object):
       pass
 
     # RGB, not BGR (might have to switch)
-    pygame.surfarray.blit_array(self.surface, img.swapaxes(0,1)[:, :, [2,1,0]]) #0,1,2
-    self.screen.blit(self.surface, (0,0))\
+    pygame.surfarray.blit_array(self.surface, img.swapaxes(0,1)[:, :, [2,1,0]]) #0,1,2 copy val from arr int o surface
+    self.screen.blit(self.surface, (0,0))   #update, top left corener
 
     # blit
     pygame.display.flip()
--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+"""
+Feature based SLAM, using ORB features
+"""
+
+
 import cv2
-import pygame
+import numpy as np
 from display import Display2D
 
 W = 1920//2
 H = 1080//2
 
 disp = Display2D(W, H)
+#print(dir(orb))
+
+class FeatureExtractor(object):
+    GX = 8
+    GY = 6
+
+    def __init__(self):
+        self.orb = cv2.ORB_create(100)
+
+    def extract(self, img):
+        feats = cv2.goodFeaturesToTrack(np.mean(img, axis = 2).astype(np.uint8), 3000, qualityLevel = 0.01, minDistance = 3)
+        self.orb.compute(img, )
+        return feats
+
+fe = FeatureExtractor()
 
 def process_frame(img):
     img = cv2.resize(img, (W, H))
+    kp= fe.extract(img)
+
+    for p in kp:
+        u,v = map(lambda x: int(round(x)), p[0])
+        cv2.circle(img, (u,v), color = (0,255,0), radius = 3) #draw circle over img
+
     disp.paint(img)
 
 if __name__ == "__main__":
