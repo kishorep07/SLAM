1769caaf174900b30093d51781ae293b3f32d833
kishorep07 <kishore.prabhakar7@gmail.com>
2019-08-19 18:27:44 +0530

    Essential Matrix Pose

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
-"""
-Feature based SLAM, using ORB features
-"""
-
-
+import time
 import cv2
-import numpy as np
 from display import Display2D
 from extractor import FeatureExtractor
+import numpy as np
 
 W = 1920//2
 H = 1080//2
 
+F = 270
 disp = Display2D(W, H)
-fe = FeatureExtractor()
+K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
+print(K)
+fe = FeatureExtractor(K)
 
 def process_frame(img):
-    img = cv2.resize(img, (W, H))
-    matches= fe.extract(img)
+  img = cv2.resize(img, (W,H))
+  matches, pose = fe.extract(img)
+  if pose is None:
+    return
 
+  print("%d matches" % (len(matches)))
+  print(pose)
 
-    for pt1, pt2 in matches:
-        u1,v1 = map(lambda x: int(round(x)), pt1)
-        u2,v2 = map(lambda x: int(round(x)), pt2)
-        cv2.circle(img, (u1,v1), color = (0,255,0), radius = 3) #draw circle over img
-        cv2.line(img, (u1,v1), (u2,v2), color = (255, 0, 0))
+  for pt1, pt2 in matches:
+    u1,v1 = fe.denormalize(pt1)
+    u2,v2 = fe.denormalize(pt2)
 
-    disp.paint(img)
+    cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
+    cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))
+
+  disp.paint(img)
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture('./videos/test_countryroad.mp4') #live capture
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret == True:
-            process_frame(frame)
-        else:
-            break
+  cap = cv2.VideoCapture("./videos/test_countryroad.mp4")
+
+  while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == True:
+      process_frame(frame)
+    else:
+      break
